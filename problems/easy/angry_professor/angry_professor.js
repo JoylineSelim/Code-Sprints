be3258fe8f352e297a0a2ce0b11d9ee35ae2e8f1
Joyline <134996568+JoylineSelim@users.noreply.github.com>
2025-05-10 12:34:36 +0300

    Update angry_professor.js

--- problems/easy/angry_professor/angry_professor.js
+++ problems/easy/angry_professor/angry_professor.js
@@ -6,8 +6,11 @@
  */
 
 function angryProfessor(k, a) {
-    // Write your code here
-    return "YES";
+    // Count how many students arrived on time (arrival time <= 0)
+    const onTime = a.filter(time => time <= 0).length;
+
+    // If the number of on-time students is less than the threshold, cancel class
+    return onTime < k ? "YES" : "NO";
 }
 
 module.exports = { angryProfessor };
