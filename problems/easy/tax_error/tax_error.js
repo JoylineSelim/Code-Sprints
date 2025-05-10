24975604223b31099cdc330b95919e38d63363ed
Joyline <134996568+JoylineSelim@users.noreply.github.com>
2025-05-10 13:12:02 +0300

    Update tax_error.js

--- problems/easy/tax_error/tax_error.js
+++ problems/easy/tax_error/tax_error.js
@@ -1,11 +1,17 @@
-function calculateTax(income) {
-    if (income < 0) {
-      return "Income can't be negative";  
+package problems.easy.tax_error;
+
+public class TaxCalculator {
+    public static double calculateTax(double income) {
+        if (income < 0) {
+            return 0.0;
+        }
+
+        double taxRate = 0.15;
+        double tax = income * taxRate; // Correct calculation
+        return tax;
     }
-    const taxRate = 0.15;
-    let tax = 0;
-    income * taxRate;  
-    return tax;
-  }
-  
-  console.log(calculateTax(10000));  
+
+    public static void main(String[] args) {
+        System.out.println(calculateTax(10000)); // Expected: 1500.0
+    }
+}
